a295e99cc0a4c6aee60fc5986b847b8ee0f64804
박수훈 <86775077+PrakSuHun@users.noreply.github.com>
2026-08-25 07:41:57 +0900

    브랜드 중립 관리자 주소 분리

--- brand-config.js
+++ brand-config.js
@@ -1,5 +1,6 @@
 // 저장소별 관리자 화면에 표시할 브랜드명입니다.
 window.BRAND_SITE = Object.freeze({
   name: "설백",
-  adminUrl: "",
+  adminUrl: "https://seolbaek-admin.progen-web.workers.dev/admin",
+  siteUrl: "https://seolbaek-tensorflow.progen-web.workers.dev",
 });
